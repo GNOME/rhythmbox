4b2446ad57cc7ccc50a42e3c92045202215a21c5
Olivier Martin <omartin@src.gnome.org>
2002-11-14 13:15:27 +0000

    fix audiocd build

--- views/rb-audiocd-view.c
+++ views/rb-audiocd-view.c
@@ -31,7 +31,7 @@
 #include <bonobo/bonobo-ui-component.h>
 #include <unistd.h>
 #include <string.h>
-#include <monkey-media/monkey-media-audio-cd.h>
+#include <monkey-media-audio-cd.h>
 
 #include "rb-stock-icons.h"
 #include "rb-node-view.h"
