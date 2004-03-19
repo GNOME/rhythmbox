26e8359f5dec51afbec1f18b286d7c649ce63b36
Christian Fredrik Kalager Schaller <uraeus@src.gnome.org>
2004-03-19 19:54:35 +0000

    fix spec file

--- autogen.sh
+++ autogen.sh
@@ -53,12 +53,12 @@ if $have_autoconf ; then : ; else
 	DIE=1
 fi
 
-if automake-1.7 --version < /dev/null > /dev/null 2>&1; then
-  AUTOMAKE=automake-1.7
-  ACLOCAL=aclocal-1.7
+if automake-1.8 --version < /dev/null > /dev/null 2>&1; then
+  AUTOMAKE=automake-1.8
+  ACLOCAL=aclocal-1.8
 else
 	echo
-	echo "You must have automake >= 1.7 installed to compile $PROJECT."
+	echo "You must have automake >= 1.8 installed to compile $PROJECT."
 	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.7.2.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
