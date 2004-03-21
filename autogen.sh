a451a4d1ce940bafd9df6949ddc72568ec4f36b4
Colin Walters <walters@src.gnome.org>
2004-03-21 18:01:28 +0000

    Revision: rhythmbox-devel@gnome.org--2004/rhythmbox--main--0.7--patch-157
    fix automake url

--- autogen.sh
+++ autogen.sh
@@ -59,7 +59,7 @@ if automake-1.8 --version < /dev/null > /dev/null 2>&1; then
 else
 	echo
 	echo "You must have automake >= 1.8 installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.7.2.tar.gz"
+	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.8.3.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
 fi
