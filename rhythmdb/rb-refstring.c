2796d0e147dc9fae160de4c063cf11565b3639aa
Colin Walters <walters@src.gnome.org>
2004-07-14 04:30:46 +0000

    continue 0.9 sync

---
+++ rhythmdb/rb-refstring.c
@@ -0,0 +1,82 @@
+/*
+ *  arch-tag: Implementation of reference-counted string
+ *
+ *  Copyright (C) 2004 Colin Walters <walters@redhat.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <config.h>
+
+#define	G_IMPLEMENT_INLINES 1
+#define	__RB_REFSTRING_C__
+
+#include <glib.h>
+#include <string.h>
+
+GHashTable *rb_refstrings;
+
+#include "rb-refstring.h"
+
+static void
+rb_refstring_free (RBRefString *refstr)
+{
+	refstr->refcount = 0xdeadbeef;
+	g_free (refstr->folded);
+	g_free (refstr->sortkey);
+	g_free (refstr);
+}
+
+void
+rb_refstring_system_init (void)
+{
+	rb_refstrings = g_hash_table_new_full (g_str_hash, g_str_equal,
+					       NULL, (GDestroyNotify) rb_refstring_free);
+}
+
+RBRefString *
+rb_refstring_new_full (const char *init, gboolean compute_sortdata)
+{
+	RBRefString *ret;
+
+	ret = g_hash_table_lookup (rb_refstrings, init);
+	if (ret) {
+		rb_refstring_ref (ret);
+		return ret;
+	}
+	
+	ret = g_malloc (sizeof (RBRefString) + strlen (init));
+	
+	ret->refcount = 1;
+	
+	if (compute_sortdata) {
+		ret->folded = g_utf8_casefold (init, -1);
+		ret->sortkey = g_utf8_collate_key (ret->folded, -1);
+	} else {
+		ret->folded = NULL;
+		ret->sortkey = NULL;
+	}
+	
+	strcpy (ret->value, init);
+	return ret;
+}
+
+void
+rb_refstring_system_shutdown (void)
+{
+	g_hash_table_destroy (rb_refstrings);
+}
+	
---
+++ rhythmdb/rb-refstring.h
@@ -0,0 +1,119 @@
+/*
+ *  arch-tag: Header for reference-counted string functions 
+ *
+ *  Copyright (C) 2004 Colin Walters <walters@verbum.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <glib.h>
+
+#ifndef __RB_REFSTRING_H
+#define __RB_REFSTRING_H
+
+typedef struct
+{
+	guint32 refcount;
+	char *folded;
+	char *sortkey;
+	char value[1];
+} RBRefString;
+
+void				rb_refstring_system_init (void);
+
+G_INLINE_FUNC RBRefString *	rb_refstring_new (const char *init);
+
+RBRefString *			rb_refstring_new_full (const char *init, gboolean compute_sortdata);
+
+G_INLINE_FUNC RBRefString *	rb_refstring_ref (RBRefString *val);
+
+G_INLINE_FUNC void		rb_refstring_unref (RBRefString *val);
+
+G_INLINE_FUNC const char *	rb_refstring_get (const RBRefString *val);
+
+G_INLINE_FUNC const char *	rb_refstring_get_folded (const RBRefString *val);
+
+G_INLINE_FUNC const char *	rb_refstring_get_sort_key (const RBRefString *val);
+
+G_INLINE_FUNC guint		rb_refstring_hash (gconstpointer a);
+
+G_INLINE_FUNC gboolean		rb_refstring_equal (gconstpointer a, gconstpointer b);
+
+void				rb_refstring_system_shutdown (void);
+
+#if defined (G_CAN_INLINE) || defined (__RB_REFSTRING_C__)
+
+#ifndef __RB_REFSTRING_C__
+extern GHashTable *rb_refstrings;
+#endif
+
+G_INLINE_FUNC RBRefString *
+rb_refstring_new (const char *init)
+{
+	return rb_refstring_new_full (init, TRUE);
+}
+
+G_INLINE_FUNC RBRefString *
+rb_refstring_ref (RBRefString *val)
+{
+	val->refcount++;
+	return val;
+}
+
+G_INLINE_FUNC void
+rb_refstring_unref (RBRefString *val)
+{
+	if (--val->refcount == 0)
+		g_hash_table_remove (rb_refstrings, val);
+}
+
+G_INLINE_FUNC const char *
+rb_refstring_get (const RBRefString *val)
+{
+	return val->value;
+}
+
+G_INLINE_FUNC const char *
+rb_refstring_get_folded (const RBRefString *val)
+{
+	return val->folded;
+}
+
+G_INLINE_FUNC const char *
+rb_refstring_get_sort_key (const RBRefString *val)
+{
+	return val->sortkey;
+}
+
+G_INLINE_FUNC guint
+rb_refstring_hash (gconstpointer p)
+{
+	const RBRefString *ref = p;
+	return g_str_hash (rb_refstring_get (ref));
+}
+
+G_INLINE_FUNC gboolean
+rb_refstring_equal (gconstpointer ap, gconstpointer bp)
+{
+	const RBRefString *a = ap;
+	const RBRefString *b = bp;
+	return g_str_equal (rb_refstring_get (a),
+			    rb_refstring_get (b));
+}
+
+#endif
+
+#endif
