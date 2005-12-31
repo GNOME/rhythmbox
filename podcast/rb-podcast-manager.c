7f1552df3efe6532475104e468b649fc15a53b5f
Jonathan Matthew <jonathan@kaolin.hn.org>
2005-12-31 09:20:38 +0000

    Include podcast posts with no publication date. Typically seen in static
    2005-12-31  Jonathan Matthew  <jonathan@kaolin.hn.org>
    
    	* podcast/rb-podcast-manager.c: (rb_podcast_manager_insert_feed):
    	Include podcast posts with no publication date.  Typically
    	seen in static collections rather than feeds.

--- podcast/rb-podcast-manager.c
+++ podcast/rb-podcast-manager.c
@@ -1505,7 +1505,7 @@ rb_podcast_manager_insert_feed (RBPodcastManager *pd, RBPodcastChannel *data)
 	for (lst_songs = data->posts; lst_songs != NULL; lst_songs = g_list_next (lst_songs)) {
 		RBPodcastItem *item = (RBPodcastItem *) lst_songs->data;
 
-		if (item->pub_date > last_post) {
+		if (item->pub_date > last_post || item->pub_date == 0) {
 			gulong status;
 			updated = TRUE;
 
