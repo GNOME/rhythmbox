7e95f6a2b59cec98a290ef1204ce1ed8606fbb8a
James Livingston <jrl@src.gnome.org>
2006-02-28 14:05:34 +0000

    fix a typo in the last commit, that makes it not work in glib 2.8 systems.
    * rhythmdb/gsequence.c: fix a typo in the last commit, that makes it
      not work in glib 2.8 systems.

--- rhythmdb/gsequence.c
+++ rhythmdb/gsequence.c
@@ -24,7 +24,7 @@
 
 #ifndef g_slice_new0
 #define g_slice_new0(a) g_new0(a, 1)
-#define g_slice_free1(a, b) g_free(b)
+#define g_slice_free(a, b) g_free(b)
 #endif
 
 typedef struct _GSequenceNode GSequenceNode;
