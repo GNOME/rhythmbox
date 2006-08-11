74060ca20d6038b60d0d0f146dbebab9a395d149
Jonathan Matthew <jonathan@kaolin.wh9.net>
2006-08-11 10:04:19 +0000

    No point allocating a buffer for the timestamp when we can just put it on
    2006-08-11  Jonathan Matthew  <jonathan@kaolin.wh9.net>
    
    	* lib/rb-debug.c: (rb_debug_real):
    	No point allocating a buffer for the timestamp when we can just put it
    	on the stack.

--- lib/rb-debug.c
+++ lib/rb-debug.c
@@ -53,10 +53,10 @@ rb_debug_real (const char *func,
 {
 	va_list args;
 	char buffer[1025];
-	char *str_time;
+	char str_time[255];
 	time_t the_time;
 
-	if (debug_match == NULL || 
+	if (debug_match == NULL ||
 	   (debug_match != debug_everything &&
 	   (strstr (file, debug_match) == NULL) &&
 	   (strstr (func, debug_match) == NULL)))
@@ -65,17 +65,14 @@ rb_debug_real (const char *func,
 	va_start (args, format);
 
 	g_vsnprintf (buffer, 1024, format, args);
-	
+
 	va_end (args);
 
 	time (&the_time);
-	str_time = g_new0 (char, 255);
 	strftime (str_time, 254, "%H:%M:%S", localtime (&the_time));
 
 	g_printerr (newline ? "(%s) [%p] [%s] %s:%d: %s\n" : "(%s) [%p] [%s] %s:%d: %s",
 		    str_time, g_thread_self (), func, file, line, buffer);
-	
-	g_free (str_time);
 }
 
 void
