c2787f0836c150382264582702abfc7a02af6d76
Jonathan Matthew <jonathan@d14n.org>
2008-10-05 12:56:22 +0000

    Don't set a non-zero page size on the spin button used for limit settings.
    2008-10-05  Jonathan Matthew  <jonathan@d14n.org>
    
    	* data/glade/create-playlist.glade:
    	Don't set a non-zero page size on the spin button used for limit
    	settings.
    	* sources/rb-auto-playlist-source.c: (impl_save_contents_to_xml):
    	Don't try to free the array holding the limit value if there isn't one.
    
    svn path=/trunk/; revision=5970

--- sources/rb-auto-playlist-source.c
+++ sources/rb-auto-playlist-source.c
@@ -710,7 +710,10 @@ impl_save_contents_to_xml (RBPlaylistSource *psource,
 
 	rhythmdb_query_serialize (rb_playlist_source_get_db (psource), query, node);
 	rhythmdb_query_free (query);
-	g_value_array_free (limit_value);
+
+	if (limit_value != NULL) {
+		g_value_array_free (limit_value);
+	}
 	g_free (sort_key);
 }
 
