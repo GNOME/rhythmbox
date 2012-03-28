a2d77c6ed7948e958690110091d64092f1d39cb1
Jonathan Matthew <jonathan@d14n.org>
2012-03-28 22:48:34 +1000

    add a function for formatting times that could be 0 seconds
    rb_make_time_string is like rb_make_duration_string, except
    it returns 0:00 for 0 seconds rather than 'Unknown'

--- lib/rb-util.h
+++ lib/rb-util.h
@@ -52,6 +52,7 @@ int rb_gvalue_compare (GValue *a, GValue *b);
 
 int rb_compare_gtimeval (GTimeVal *a, GTimeVal *b);
 int rb_safe_strcmp (const char *a, const char *b);
+char *rb_make_time_string (guint seconds);
 char *rb_make_duration_string (guint duration);
 char *rb_make_elapsed_time_string (guint elapsed, guint duration, gboolean show_remaining);
 
