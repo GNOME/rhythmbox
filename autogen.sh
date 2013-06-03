67899c221cab2c26ecbaafc0845b2818c2518fc6
Jonathan Matthew <jonathan@d14n.org>
2013-06-03 21:02:32 +1000

    autogen: fail noisily if yelp-tools can't be found

--- autogen.sh
+++ autogen.sh
@@ -30,6 +30,13 @@ if test -z `which autopoint`; then
         exit 1
 fi
 
+
+ACDIR=`${ACLOCAL:-aclocal} --print-ac-dir`
+if ! test -f $ACDIR/yelp.m4; then
+	echo "*** No yelp-tools found, please install it ***"
+	exit 1
+fi
+
 # Need to mkdir -p the m4 directory in case it doesn't exist, to prevent
 # gtkdocize from failing.
 mkdir -p m4
