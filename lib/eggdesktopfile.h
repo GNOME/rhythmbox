9c86a476f5cb79795ac83f15e831f8128c13904e
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-01-23 12:44:44 +0100

    Updated FSF's address

--- lib/eggdesktopfile.h
+++ lib/eggdesktopfile.h
@@ -13,8 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; see the file COPYING.LIB. If not,
- * write to the Free Software Foundation, Inc., 59 Temple Place -
- * Suite 330, Boston, MA 02111-1307, USA.
+ * see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __EGG_DESKTOP_FILE_H__
