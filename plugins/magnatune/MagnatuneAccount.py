f370706edfedffda1e74d80b806db73a36c5a54e
Jonathan Matthew <jonathan@d14n.org>
2014-08-03 16:20:27 +1000

    require libsecret 0.18; formerly unstable API is now stable
    https://bugzilla.gnome.org/show_bug.cgi?id=725884

--- plugins/magnatune/MagnatuneAccount.py
+++ plugins/magnatune/MagnatuneAccount.py
@@ -34,7 +34,7 @@ if rbconfig.libsecret_enabled:
 	# Till libsecret completely replaces gnome-keyring, we'll fall back to not
 	# saving the password if libsecret can't be found. This code can be removed later.
 	try:
-		from gi.repository import Secret, SecretUnstable
+		from gi.repository import Secret
 		# We need to be able to fetch passwords stored by libgnome-keyring, so we use
 		# a schema with SECRET_SCHEMA_DONT_MATCH_NAME set.
 		# See: http://developer.gnome.org/libsecret/unstable/migrating-schemas.html
@@ -65,10 +65,10 @@ class MagnatuneAccount(object):
 			print ("Account details will not be saved because libsecret was not found")
 			return
 		# Haha.
-		self.secret_service = SecretUnstable.Service.get_sync(SecretUnstable.ServiceFlags.OPEN_SESSION, None)
+		self.secret_service = Secret.Service.get_sync(Secret.ServiceFlags.OPEN_SESSION, None)
 		items = self.secret_service.search_sync(MAGNATUNE_SCHEMA,
 							self.keyring_attributes,
-							SecretUnstable.SearchFlags.LOAD_SECRETS,
+							Secret.SearchFlags.LOAD_SECRETS,
 							None)
 		if not items:
 			# The Python API doesn't seem to have a way to differentiate between errors and no results.
