9b6a6b641c5f8f15c5ae7e8a5cfe8b2fb2be7fbb
Jonathan Matthew <jonathan@d14n.org>
2015-02-22 23:33:06 +1000

    file-helpers: remove obsolete 'art' dirs from search paths

--- lib/rb-file-helpers.c
+++ lib/rb-file-helpers.c
@@ -66,18 +66,14 @@ static char *user_cache_dir = NULL;
 static char *uninstalled_paths[] = {
 	SHARE_UNINSTALLED_DIR "/",
 	SHARE_UNINSTALLED_DIR "/ui/",
-	SHARE_UNINSTALLED_DIR "/art/",
 	SHARE_UNINSTALLED_BUILDDIR "/",
 	SHARE_UNINSTALLED_BUILDDIR "/ui/",
-	SHARE_UNINSTALLED_BUILDDIR "/art/",
 	SHARE_DIR "/",
-	SHARE_DIR "/art/",
 	NULL
 };
 
 static char *installed_paths[] = {
 	SHARE_DIR "/",
-	SHARE_DIR "/art/",
 	NULL
 };
 
