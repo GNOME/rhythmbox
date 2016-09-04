ce0d3910d9a7909dea0ae12753d72c9edb68e96a
Jonathan Matthew <jonathan@d14n.org>
2016-09-04 13:52:05 +1000

    audioscrobbler: use https for last.fm
    https://bugzilla.gnome.org/show_bug.cgi?id=768310

--- plugins/audioscrobbler/rb-audioscrobbler-service.c
+++ plugins/audioscrobbler/rb-audioscrobbler-service.c
@@ -30,10 +30,10 @@
 
 /* Last.fm details */
 #define LASTFM_NAME "Last.fm"
-#define LASTFM_AUTH_URL "http://www.last.fm/api/auth/"
-#define LASTFM_SCROBBLER_URL "http://post.audioscrobbler.com/"
-#define LASTFM_API_URL "http://ws.audioscrobbler.com/2.0/"
-#define LASTFM_OLD_RADIO_API_URL "http://ws.audioscrobbler.com/"
+#define LASTFM_AUTH_URL "https://www.last.fm/api/auth/"
+#define LASTFM_SCROBBLER_URL "https://post.audioscrobbler.com/"
+#define LASTFM_API_URL "https://ws.audioscrobbler.com/2.0/"
+#define LASTFM_OLD_RADIO_API_URL "https://ws.audioscrobbler.com/"
 /* this API key belongs to Jamie Nicol <jamie@thenicols.net>
    generated May 2010 for use in the audioscrobbler plugin */
 #define LASTFM_API_KEY "0337ff3c59299b6a31d75164041860b6"
