4d6b381c29cbc3dd18b9a6758cc9a96ec8d97e05
Jonathan Matthew <jonathan@d14n.org>
2025-12-01 07:40:24 +1000

    lyrics: fetch lyrics from lrclib.net
    Also make this the default lyrics search, since it seems to cover
    pretty mainstream music whereas the other remaining sites are focused
    on a specific genre or language.

--- plugins/lyrics/LrclibParser.py
+++ plugins/lyrics/LrclibParser.py
@@ -1,6 +1,6 @@
 # -*- Mode: python; coding: utf-8; tab-width: 8; indent-tabs-mode: t; -*-
 #
-# Copyright (C) 2009 Jonathan Matthew
+# Copyright (C) 2025 Jonathan Matthew
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
 # obligated to do so. If you do not wish to do so, delete this exception
 # statement from your version.
 #
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -25,20 +26,45 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
 
 
-from TerraParser import TerraParser
-from DarkLyricsParser import DarkLyricsParser
-from JlyricParser import JlyricParser
-from LrclibParser import LrclibParser
+import urllib.parse
+import rb
+import sys
+import json
 
-from gi.repository import RB
+class LrclibParser (object):
+	def __init__(self, artist, title):
+		self.artist = artist
+		self.title = title
 
-import gettext
-gettext.install('rhythmbox', RB.locale_dir())
+	def search(self, callback, *data):
+		# lrclib.net/api/search?track_name=x&artist_name=y
 
-lyrics_sites = [
-	{ 'id': 'lrclib.net',		'class': LrclibParser,		'name': _("LRCLIB (lrclib.net)")		},
-	{ 'id': 'terra.com.br',		'class': TerraParser,		'name': _("TerraBrasil (terra.com.br)")		},
-	{ 'id': 'darklyrics.com',	'class': DarkLyricsParser,	'name': _("Dark Lyrics (darklyrics.com)")	},
-	{ 'id': 'j-lyric.net', 		'class': JlyricParser, 		'name': _("Jlyric (j-lyric.net)") 		},
-]
+		q = urllib.parse.urlencode({
+			'track_name': self.title,
+			'artist_name': self.artist
+		})
+		url = urllib.parse.urlunparse(('https', 'lrclib.net', 'api/search', '', q, ''))
+
+		loader = rb.Loader()
+		loader.get_url (url, self.got_lyrics, callback, *data)
+
+	def got_lyrics(self, result, callback, *data):
+		if result is None:
+			callback (None, *data)
+			return
+
+		if result is not None:
+			j = json.loads(result)
+			# just take the first one?
+			if isinstance(j, list) and len(j) > 0:
+				l = j[0]
+				lyrics = l.get('plainLyrics')
+				lyrics += "\n\nLyrics provided by lrclib.net"
+
+				callback (lyrics, *data)
+			else:
+				callback (None, *data)
+		else:
+			callback (None, *data)
+
 
